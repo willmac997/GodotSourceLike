d2e3c2d49e0d12f9b2688e2bfc6697129caeee97
Will <will@upshift.co.nz>
2024-09-17 15:21:14 +1200

    console wip

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -4,6 +4,10 @@ public partial class Game : Node3D
 {
   public override void _Process(double delta)
   {
-    if (Input.IsActionJustPressed("Escape")) GetTree().Quit();
+	if (Input.IsActionJustPressed("Escape"))
+	{
+	  if (Input.MouseMode == Input.MouseModeEnum.Visible) GetTree().Quit();
+	  Input.MouseMode = Input.MouseModeEnum.Visible;
+	}
   }
 }
