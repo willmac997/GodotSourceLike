56f4113106290adbeed2f7df9af8d030c3629fe9
Will <will@upshift.co.nz>
2025-02-02 09:03:28 +1300

    added debug draw addon

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -5,12 +5,12 @@ public partial class Game : Node3D
   private static bool _console = false;
   public static bool Console
   {
-    get { return _console; }
-    set { _console = value; }
+	get { return _console; }
+	set { _console = value; }
   }
 
   public override void _Process(double delta)
   {
-    if (Input.IsActionJustPressed("Escape")) GetTree().Quit();
+	if (Input.IsActionJustPressed("Escape")) GetTree().Quit();
   }
 }
